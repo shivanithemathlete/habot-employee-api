71a641c1c4c42e94efa91fb6a67cb095e89369ba
Shivani Sharma <shivani@Shivanis-MacBook-Air.local>
2026-01-14 19:49:11 +0530

    DB session dependency

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 DATABASE_URL = "sqlite:///./employees.db"
 
@@ -14,3 +14,10 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
